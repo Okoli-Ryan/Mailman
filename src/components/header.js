ec4795bfaabe95017afa7f6aa295c54e2e06c75c
okoli-ryan50 <okoliryan50@gmail.com>
2020-07-18 20:39:15 +0100

    can view room name at top of screen

--- src/components/header.js
+++ src/components/header.js
@@ -4,22 +4,29 @@ import { useDispatch, useSelector } from "react-redux";
 const Header = () => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.loginReducer);
+  const room = useSelector((state) => state.menuBarReducer);
 
   return (
     <div className="header">
-      <div className="menu-button-container">
-        {user.email && (
-          <button
-            onClick={() => dispatch({ type: "display" })}
-            className="menubar"
-          ></button>
-        )}
+      <div className="header-content">
+        <div className="menu-button-container">
+          {user.email && (
+            <button
+              onClick={() => dispatch({ type: "display" })}
+              className="menubar"
+            ></button>
+          )}
+        </div>
+        <div className="app-name-container">
+          <p>M@ilman</p>
+        </div>
+        <div></div>
       </div>
-      <div className="app-name-container">
-        <p>M@ilman</p>
-      </div>
-      <div>
-      </div>
+      {room !== "empty" && (
+        <div className="room-name">
+          <p>{room}</p>
+        </div>
+      )}
     </div>
   );
 };
